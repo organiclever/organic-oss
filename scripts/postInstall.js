a480293595583f487f74fd1af779f2fbf8767bcc
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2022-01-16 06:29:37 +0700

    [Repo] Add pre and post install to autogenerate nx.json

---
+++ scripts/postInstall.js
@@ -0,0 +1,22 @@
+const { exec } = require('child_process');
+
+console.log('Running post-install script');
+
+const prettifyList = ['./nx.json'];
+
+exec(
+  `yarn prettier --write ${prettifyList.join(' ')}`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  }
+);
+
+console.log('Finished running post-install script');
---
+++ scripts/preInstall.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const NX_JSON_TEMPLATE = '../generator/template/nx-json-template.json';
+const NX_JSON_TARGET = './nx.json';
+
+const cred = require('../organic-cred.json');
+const nxConfig = require(NX_JSON_TEMPLATE);
+
+console.log('Running pre-install script');
+
+const accessToken = cred.accessToken;
+
+const newNxConfig = { ...nxConfig, accessToken };
+
+fs.writeFile(NX_JSON_TARGET, JSON.stringify(newNxConfig), (err) => {
+  if (err) {
+    console.log('Error writing file', err);
+  } else {
+    console.log('Successfully wrote file');
+  }
+});
