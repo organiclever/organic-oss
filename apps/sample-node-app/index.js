5dc413d3331c2dc3827f3ed542bb9e854788d4c6
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2022-03-06 16:52:40 +0700

    [repo] test - nx cache

--- apps/sample-node-app/index.js
+++ apps/sample-node-app/index.js
@@ -1,4 +1,4 @@
 const sumBy5 = require('@hall/complex-node-lib');
 
-console.log('Hello from sample-node-app');
+console.log('Hello from sample-node-app!');
 console.log('this is the sum of 2 and 5 is : ', sumBy5(2));
